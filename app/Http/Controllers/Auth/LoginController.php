54f8798ca2795d2b694bc25df287380e2c7eafda
lnlockly <lnlockly@gmail.com>
2022-06-04 16:08:58 +0300

    login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Azate\LaravelTelegramLoginAuth\TelegramLoginAuth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -20,7 +22,14 @@ class LoginController extends Controller
     */
 
     use AuthenticatesUsers;
+    public function handleTelegramCallback(TelegramLoginAuth $telegramLoginAuth, Request $request)
+    {
+        if ($user = $telegramLoginAuth->validate($request)) {
+            dd($user);
+        }
 
+        // ...
+    }
     /**
      * Where to redirect users after login.
      *
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
                 <div class="card-body">
-                    <script async src="https://telegram.org/js/telegram-widget.js?19" data-telegram-login="testbotobot123" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+                    <script async src="https://telegram.org/js/telegram-widget.js?19" data-telegram-login="chipboto_bot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
                     <script type="text/javascript">
                     function onTelegramAuth(user) {
                         alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
