18900c9887b1968629321e5fd9c098d1aa81dfad
lnlockly <lnlockly@gmail.com>
2022-06-04 15:17:55 +0300

    1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,21 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
-
                 <div class="card-body">
-         
+                    <script async src="https://telegram.org/js/telegram-widget.js?19" data-telegram-login="testbotobot123" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+                    <script type="text/javascript">
+                    function onTelegramAuth(user) {
+                        alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+                    }
+
+                    </script>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<script async src="https://telegram.org/js/telegram-widget.js?19" data-telegram-login="testbotobot123" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
-<script type="text/javascript">
-  function onTelegramAuth(user) {
-    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-  }
-</script>
 @endsection
