fb6cd4b74c22f79d08b4bd25475c8cda9de1d57c
lnlockly <lnlockly@gmail.com>
2022-06-04 15:14:54 +0300

    fix some and telegram login widget

--- app/Http/Middleware/IfShop.php
+++ app/Http/Middleware/IfShop.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Route;
 
 use Closure;
 
@@ -15,7 +16,7 @@ class IfShop
      */
     public function handle($request, Closure $next)
     {
-        if(!isset(auth()->user()->shop)) {
+        if(!isset(auth()->user()->shop) && Route::current()->getName() != "shop.create") {
             return redirect(route('shop.create'));
         }
         return $next($request);
--- database/migrations/2022_05_30_213952_create_shops_table.php
+++ database/migrations/2022_05_30_213952_create_shops_table.php
@@ -20,7 +20,7 @@ class CreateShopsTable extends Migration
             $table->string('language');
             $table->string('currency');
             $table->string('timezone');
-            $table->integer('user_id')->unsigned();
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
