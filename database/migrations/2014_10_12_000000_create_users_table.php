328f6647232a24050ce436558a66e10c8194966e
lnlockly <lnlockly@gmail.com>
2022-06-08 15:41:37 +0300

    1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('telegram_id');
+            $table->string('telegram_id')->default('0');
             $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
