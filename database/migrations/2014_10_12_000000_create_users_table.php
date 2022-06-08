7d010bea2a1d6342cffdd28461f6958ee3c3a12a
lnlockly <lnlockly@gmail.com>
2022-06-08 18:51:54 +0300

    1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
             $table->string('telegram_id')->default('0');
             $table->boolean('is_admin')->default(false);
