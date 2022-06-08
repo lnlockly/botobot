436660dacf58303ab9f41816df5cbb112a146202
lnlockly <lnlockly@gmail.com>
2022-06-08 18:15:27 +0300

    1

--- app/Http/Controllers/Auth/TelegramAuth.php
+++ app/Http/Controllers/Auth/TelegramAuth.php
@@ -9,8 +9,27 @@ use App\User;
 
 class TelegramAuth extends Controller
 {
+
+    /**
+     * @var TelegramLoginAuth
+     */
+    protected $telegram;
+
+    /**
+     * AuthController constructor.
+     *
+     * @param TelegramLoginAuth $telegram
+     */
+    public function __construct(TelegramLoginAuth $telegram)
+    {
+        $this->telegram = $telegram;
+    }
+
+
     public function callback(TelegramLoginAuth $telegramLoginAuth) {
-        if ($user = $telegramLoginAuth->validate()) {
+         if ($this->telegram->validate()) {
+            $user = $this->telegram->user();
+
 
             if (User::where(['telegram_id' => $user->id]) != null){
                 Auth::login(User::where(['telegram_id' => $user->id]));
