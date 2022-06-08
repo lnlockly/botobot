883ce0bc7656d24cf528f769c2e74cd09951355a
lnlockly <lnlockly@gmail.com>
2022-06-08 16:06:02 +0300

    1

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -15,13 +15,13 @@ class BotController extends Controller
 {
     public function index($token) {
         $message = Telegram::getWebhookUpdates();
-        $bot->sendMessage([
+   
+
+        $bot = new Api($token);
+     $bot->sendMessage([
           'chat_id' => 1361064246, 
           'text' => $message,
         ]);
-
-        $bot = new Api($token);
-
         $shop = Shop::where(['bot_token' => $token])->get();
 
         if (isset($message->message->from)) {
