f599fc1fc3bbc1c0174a163d23098abbded29b55
lnlockly <lnlockly@gmail.com>
2022-06-28 23:47:19 +0300

    1

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = 'https://chipbot.ru/create';
 
     /**
      * Create a new controller instance.
