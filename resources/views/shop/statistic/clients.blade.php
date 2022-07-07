2207b5620eb885c4d69a27a2e5e64beecec00efc
kostergg <kosterin2741@mail.ru>
2022-07-07 11:53:42 +0300

    end shop.blade

--- resources/views/shop/statistic/clients.blade.php
+++ resources/views/shop/statistic/clients.blade.php
@@ -3,7 +3,7 @@
 <div class="col-lg-12">
   <div class="card">
     <div class="card-body">
-      <h4 class="card-title">Клиенты</h4>
+      <h4 class="card-title">Мои клиенты</h4>
        @livewire('clients-table-view')
     </div>
   </div>
