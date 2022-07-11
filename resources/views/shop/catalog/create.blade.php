42b7cc7e1b693a802d9c056d205dac446c8843cb
lnlockly <lnlockly@gmail.com>
2022-07-11 20:49:28 +0300

    VSE KAK BILO

--- resources/views/shop/catalog/create.blade.php
+++ resources/views/shop/catalog/create.blade.php
@@ -1,27 +1,5 @@
 @extends('layouts.shop')
 @section('content')
-@if(isset($message))
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-@endif
-
 <div class="grid-margin stretch-card">
     <div class="card">
         <div class="card-body">
