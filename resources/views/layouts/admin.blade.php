3eb4c5be8c61122c4e69032b07929b24780bee8d
lnlockly <lnlockly@gmail.com>
2022-07-19 19:05:13 +0300

    123

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,105 +17,130 @@
     <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
     <!-- endinject -->
     @laravelViewsStyles
-
+    @notifyCss
 </head>
 
 <body>
-    <!-- partial:partials/_navbar.html -->
-    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-          <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-              <a class="navbar-brand brand-logo me-5"><img src="{{ asset('/images/icon.svg') }}"
-                                                           class="me-2" alt="logo" /></a>
-              <a class="navbar-brand brand-logo-mini"><img src="{{ asset('/images/icon.svg') }}" /></a>
-          </div>
-      </div>
+<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+    <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo me-5"><img src="{{ asset('/images/icon.svg') }}"
+                                                     class="me-2" alt="logo" /></a>
+        <a class="navbar-brand brand-logo-mini"><img src="{{ asset('/images/icon.svg') }}" /></a>
+    </div>
+    <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <ul class="navbar-nav navbar-nav-right" style="margin-right:10px">
+
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
+                data-toggle="offcanvas">
+            <span class="ti-view-list"></span>
+        </button>
+    </div>
+</nav>
+<!-- partial -->
+<div class="container-fluid page-body-wrapper">
+    <!-- partial:partials/_sidebar.html -->
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.users') }}">
+                    <span class="menu-title">Пользователи</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.mailing.create') }}">
+                    <span class="menu-title">Рассылка</span>
+                </a>
+            </li>
+        </ul>
     </nav>
     <!-- partial -->
-    <div class="container-fluid page-body-wrapper">
-      <!-- partial:partials/_sidebar.html -->
-      <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('admin.users') }}">
-              <span class="menu-title">Пользователи</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('admin.mailing.create') }}">
-              <span class="menu-title">Рассылка</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- partial -->
-      <div class="main-panel">
+    <div class="main-panel">
         <div class="content-wrapper">
-          @yield('content')
+            @yield('content')
         </div>
         <!-- content-wrapper ends -->
         <!-- partial:partials/_footer.html -->
         <footer class="footer">
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.bootstrapdash.com/" target="_blank">bootstrapdash.com </a>2021</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Only the best <a href="https://www.bootstrapdash.com/" target="_blank"> Bootstrap dashboard </a> templates</span>
-          </div>
+
         </footer>
         <!-- partial -->
-      </div>
-      <!-- main-panel ends -->
     </div>
-    <!-- page-body-wrapper ends -->
+    <!-- main-panel ends -->
+</div>
+<!-- page-body-wrapper ends -->
+<!-- container-scroller -->
+@laravelViewsScripts
+<!-- plugins:js -->
+<script src="vendors/base/vendor.bundle.base.js"></script>
 
-    <footer>
-        <div class="wraper">
-            <div class="footer-block">
+<!-- inject:js -->
+<script src="{{ asset('js/off-canvas.js') }}"></script>
+<script src="{{ asset('js/hoverable-collapse.js') }}"></script>
+<script src="{{ asset('js/template.js') }}"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="{{ asset('js/dashboard.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+<!-- End custom js for this page-->
+<!-- footer -->
+<footer>
+    <div class="wraper">
+        <div class="footer-block">
+        </div>
+        <div class="footer-links">
+            <div class="footer-links-logo">
+
             </div>
-            <div class="footer-links">
-                <div class="footer-links-logo">
+            <div class="footer-link1">
+                <div class="footer-padding"><h1>Chipbot</h1></div>
+                <ul>
+                    <li><a href="#">О компании</a></li>
+                    <li><a href="#">Контакты</a></li>
+                    <li><a href="#">Новости</a></li>
+                    <li><a href="#">Пользовательское соглашение</a></li>
+                </ul>
+            </div>
+            <div class="footer-link2">
+                <div class="footer-padding"><h1>Услуги</h1></div>
+                <div class="footer-ul"><ul>
+                        <li><a href="#">Telegram магазина под ключ</a></li>
+                        <li><a href="#">Интеграция интернет-магазина с telegram</a></li>
+                    </ul></div>
+            </div>
+            <div class="footer-link3">
+                <div class="footer-padding"><h1>Помощь</h1></div>
+                <div class="footer-ul"><ul>
+                        <li><a href="#">База знаний</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </ul></div>
+            </div>
+            <div class="footer-link4">
+                <div class="footer-padding"><h1>Способы оплаты</h1></div>
+                <div class="footer-ul">
 
-                </div>
-                <div class="footer-link1">
-                    <div class="footer-padding"><h1>Chipbot</h1></div>
+                    <a href="#">Подробнее о способах оплаты</a>
                     <ul>
-                        <li><a href="#">О компании</a></li>
-                        <li><a href="#">Контакты</a></li>
-                        <li><a href="#">Новости</a></li>
-                        <li><a href="#">Пользовательское соглашение</a></li>
+                        <li><h6>ИП Иванцов А.А.</h6></li>
+                        <li><h6>ИНН: 616806543687</h6></li>
+                        <li><h6>ОГРНИП:321619600227803</h6></li>
+                        <li><h6>ОКПО: 2012163842</h6></li>
                     </ul>
-                </div>
-                <div class="footer-link2">
-                    <div class="footer-padding"><h1>Услуги</h1></div>
-                    <div class="footer-ul"><ul>
-                            <li><a href="#">Telegram магазина под ключ</a></li>
-                            <li><a href="#">Интеграция интернет-магазина с telegram</a></li>
-                        </ul></div>
-                </div>
-                <div class="footer-link3">
-                    <div class="footer-padding"><h1>Помощь</h1></div>
-                    <div class="footer-ul"><ul>
-                            <li><a href="#">База знаний</a></li>
-                            <li><a href="#">FAQ</a></li>
-                        </ul></div>
-                </div>
-                <div class="footer-link4">
-                    <div class="footer-padding"><h1>Способы оплаты</h1></div>
-                    <div class="footer-ul">
-
-                        <a href="#">Подробнее о способах оплаты</a>
-                        <ul>
-                            <li><h6>ИП Иванцов А.А.</h6></li>
-                            <li><h6>ИНН: 616806543687</h6></li>
-                            <li><h6>ОГРНИП:321619600227803</h6></li>
-                            <li><h6>ОКПО: 2012163842</h6></li>
-                        </ul>
-                    </div>
                 </div>
             </div>
         </div>
-    </footer>
-
+    </div>
+</footer>
+<x:notify-messages />
+@notifyJs
+<style>
+    .notify {
+        z-index: 1000000;
+        align-items: flex-end;
+    }
+</style>
 </body>
+
 
 </html>
 
